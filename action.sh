018177f69a6aa3611ba6b014a162f576eb07cb2d
Rafa Leo <rafael.leonardo.cruz@outlook.com>
2025-05-13 15:07:58 -0300

    fix: pin netlify-clo to v20

--- action.sh
+++ action.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 echo "Running Netlify CLI command..."
-echo "> npx netlify-cli $*"
 
-set -e
-OUTPUT=$(bash -c "npx netlify-cli $*" | tr '\n' ' ')
-set +e
+npx netlify-cli@20 --help
+
+echo "> npx netlify-cli@20 $*"
+
+set -o pipefail
+OUTPUT=$(bash -c "npx netlify-cli@20 $*" | tr '\n' ' ') || exit $?
+set +o pipefail
+
+echo "Done!"
 
 NETLIFY_OUTPUT=$(echo "$OUTPUT")
 
